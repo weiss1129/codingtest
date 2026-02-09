5b3965447ff436b839330ac7cc3d4640d702f6c1
BAEK\weiss <weiss1129@gmail.com>
2026-02-09 15:26:48 +0900

    P12891 import fix

--- codingtest/src/main/P12891.java
+++ codingtest/src/main/P12891.java
@@ -3,8 +3,6 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
